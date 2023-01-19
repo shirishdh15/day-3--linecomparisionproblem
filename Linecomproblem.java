dfd10a309ad7a887f41e5f2fce569096d62be735
shirish <shirishdhalgade15@gmail.com>
2023-01-19 18:09:34 +0530

    UC2

--- Linecomproblem.java
+++ Linecomproblem.java
@@ -1,5 +1,6 @@
 package Linecomproblem;
 import java.util.Scanner;
+
 public class Linecomproblem {
     public static void length() {
         System.out.println("WELCOME TO LINE COMPARISION COMPUTATION PROGRAM");
@@ -12,8 +13,29 @@ public class Linecomproblem {
         float len = (float) Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
         System.out.println("length of the line: " +len);
     }
-    public static void main(String[] args) {
+    public static void lengthEquals( ){
+        System.out.println("Enter the co-ordinates of first line: ");
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+        System.out.println("Enter the co-ordinates of second line: ");
+        int x12 = sc.nextInt();
+        int y12 = sc.nextInt();
+        int x22 = sc.nextInt();
+        int y22 = sc.nextInt();
+        Double len1 = (double)Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+        Double len2 = (double)Math.sqrt((x12-x22)*(x12-x22)+(y12-y22)*(y12-y22));
+        System.out.println("len1: "+len1);
+        System.out.println("len2: "+len2);
+        if(len1.equals(len2)){
+            System.out.println("Both lines are equal");
+        }else {
+            System.out.println("Both lines are Not equal");
+        }
+    }public static void main(String[] args) {
         length();
-
+        lengthEquals();
     }
 }
