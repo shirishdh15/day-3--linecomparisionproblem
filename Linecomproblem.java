7e203a30ed0953c39f8d8d6640b1a5ffeedfceb9
shirish <shirishdhalgade15@gmail.com>
2023-01-19 18:16:22 +0530

    UC3

--- Linecomproblem.java
+++ Linecomproblem.java
@@ -12,6 +12,7 @@ public class Linecomproblem {
         int y2 = sc.nextInt();
         float len = (float) Math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
         System.out.println("length of the line: " +len);
+
     }
     public static void lengthEquals( ){
         System.out.println("Enter the co-ordinates of first line: ");
@@ -34,8 +35,28 @@ public class Linecomproblem {
         }else {
             System.out.println("Both lines are Not equal");
         }
-    }public static void main(String[] args) {
+    }
+    public static void lengthCompare(){
+        System.out.println("Enter Values to check the lines: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter len1 value: ");
+        double len1 = sc.nextInt();
+        System.out.println("Enter len2 value: ");
+        double len2 = sc.nextInt();
+        if(Double.compare(len1,len2) == 0){
+            System.out.println("len1=len2");
+        }else if (Double.compare(len1,len2) < 0){
+            System.out.println("len1 is less than len2");
+        }else {
+            System.out.println("len1 is greater than len2");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
         length();
         lengthEquals();
+        lengthCompare();
     }
 }
